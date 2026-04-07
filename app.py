c0a7b028eb9a6f5a7e5d4a62dcfb883d43a081fa
vvce23ise0094-hue <vvce23ise0094@vvce.ac.in>
2026-04-07 15:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Docker is running this Python app successfully "
+    return "Hello! Docker is running this Python  successfully "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
